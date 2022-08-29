3f24d2ff12016259c8df6e832af3953331d2012b
Lavry <dinamartinezruiz@gmail.com>
2022-08-29 17:04:38 +0200

    Hola Mundo

--- PrimeraAplicacionGIT/src/primeraaplicaciongit/PrimeraAplicacionGIT.java
+++ PrimeraAplicacionGIT/src/primeraaplicaciongit/PrimeraAplicacionGIT.java
@@ -9,7 +9,7 @@ package primeraaplicaciongit;
  * @author dinamartinezruiz
  */
 public class PrimeraAplicacionGIT
-{
+{ 
 
     /**
      * @param args the command line arguments
@@ -17,6 +17,7 @@ public class PrimeraAplicacionGIT
     public static void main(String[] args)
     {
         // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
